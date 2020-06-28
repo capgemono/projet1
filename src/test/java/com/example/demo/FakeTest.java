74d8469ea0cd9e3ad5a87595043c06496b107808
elba <elba@192.168.1.14>
2020-06-28 12:04:06 +0200

    mon 1er commit

---
+++ src/test/java/com/example/demo/FakeTest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class FakeTest {
+
+	@Test
+	void fakeFct() {
+		assertEquals(4, 2*2);
+	}
+}
